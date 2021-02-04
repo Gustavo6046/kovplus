8440b2580e6ab5a13d76da08224ff62dca43d912
Gustavo6046 <rehermann6046@gmail.com>
2021-02-04 04:30:40 -0300

    Added support for parsing files before using the kovplus demo

--- demo.cpp
+++ demo.cpp
@@ -1,13 +1,34 @@
 #include "kovplus.hpp"
 
 #include <iostream>
+#include <fstream>
 
 
-int main() {
+int main(int argc, char **argv) {
 	KovPlusChain my_chain(10);
 
 	std::default_random_engine rng;
 
+	int line_count;
+
+	// parse passed files
+	for (int i = 0; i < argc; i++) {
+		char *arg = argv[i];
+
+		std::ifstream my_file;
+		my_file.open(arg, std::ios::in);
+
+		for (std::string line; std::getline(std::cin, line);) {
+			my_chain.add_sentence(line);
+			line_count++;
+		}
+	}
+
+	if (argc) {
+		std::cerr << "Parsed " << line_count << " lines, now operating on stdin and stdout.";
+	}
+
+	// main repl loop
 	for (std::string line; std::getline(std::cin, line);) {
 		my_chain.add_sentence(line);
 
